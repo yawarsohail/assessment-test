e51081f441f54f6c198963c054cbc67286a12244
Guest <temp_guest@macbooks-macbook-pro.local>
2018-07-10 19:24:45 +0500

    Initial Commit

---
+++ assessment-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  assessment-test
+//
+//  Created by Guest on 10/07/2018.
+//  Copyright © 2018 Guest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ assessment-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  assessment-test
+//
+//  Created by Guest on 10/07/2018.
+//  Copyright © 2018 Guest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
